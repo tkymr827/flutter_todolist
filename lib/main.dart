37416b2cce693ea20043c5fa09159306fe68656c
Ryoya Takayama <takayamar827@gmail.com>
2021-03-14 00:35:55 +0900

    add:　画面遷移できるように

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todolist/post.dart';
 
 void main() => runApp(Myapp());
 
 class Myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: "一覧",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("一覧"),
-            actions: [IconButton(icon: Icon(Icons.add))],
-            centerTitle: true,
-          ),
-          body: ListView(
-            children: [
-              Card(child: ListTile(title: Todos())),
-              Card(child: ListTile(title: Text("hoge"))),
-              Card(child: ListTile(title: Text("hoge"))),
-            ],
-          ),
-        ));
+    return MaterialApp(title: "一覧", home: Contents()
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //     title: Text("一覧"),
+        //     actions: [
+        //       IconButton(
+        //         icon: Icon(Icons.add),
+        //         onPressed: () {
+        //           Navigator.push(
+        //               context,
+        //               MaterialPageRoute(
+        //                 builder: (context) => Post(),
+        //               ));
+        //         },
+        //       )
+        //     ],
+        //     centerTitle: true,
+        //   ),
+        //   body: ListView(
+        //     children: [
+        //       Card(child: ListTile(title: Todos())),
+        //       Card(child: ListTile(title: Text("hoge"))),
+        //       Card(child: ListTile(title: Text("hoge"))),
+        //     ],
+        //   ),
+        // ),
+        );
+  }
+}
+
+class Contents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("一覧 contents"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Post()),
+                );
+              },
+            )
+          ],
+        ),
+        body: Container(child: Text("body contents")));
   }
 }
 
---
+++ lib/post.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Post extends StatefulWidget {
+  @override
+  _PostState createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("投稿"),
+      ),
+      body: Container(
+        child: Text("hoge"),
+      ),
+    );
+  }
+}
