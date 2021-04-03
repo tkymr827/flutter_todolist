05df5eacbce20f4b70eec7b00ae26cdbe36b5b26
Ryoya Takayama <takayamar827@gmail.com>
2021-04-03 18:09:13 +0900

    add: 投稿画面に各入力欄を追加

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class Post extends StatefulWidget {
-  @override
-  _PostState createState() => _PostState();
-}
-
-class _PostState extends State<Post> {
+class Post extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("投稿"),
-      ),
-      body: Container(
-        child: Text("hoge"),
+      appBar: AppBar(title: Text('投稿')),
+      body: FormTemplate(),
+    );
+  }
+}
+
+class FormTemplate extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: [
+          TextField(
+            maxLength: 20,
+            decoration: InputDecoration(labelText: 'タイトル'),
+          ),
+          TextField(
+            maxLength: 10,
+            maxLines: 3,
+            decoration: InputDecoration(labelText: '内容'),
+          ),
+          TextField(
+            maxLength: 4,
+            maxLines: 1,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(labelText: '削除用パスワード'),
+            obscureText: true,
+          ),
+          RaisedButton(
+            onPressed: () {
+              print('submit');
+            },
+            child: Text('submit'),
+          )
+        ],
       ),
     );
   }
